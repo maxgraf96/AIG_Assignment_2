75b7d6f6e7c9fe3a92d5a9c9afb209b93274d9ba
Sajid <sajidalam786@gmail.com>
2020-12-10 11:29:34 +0000

    Linear Sarsa update 2
    Getting outputs now but they are very large.

--- NonTabularModelFree/LinearSarsa.py
+++ NonTabularModelFree/LinearSarsa.py
@@ -2,12 +2,12 @@ import numpy as np
 
 
 # epsilon-greedy exploration strategy
-def epsilon_greedy(state, epsilon1, n_actions):
+def epsilon_greedy(q, epsilon1, n_actions):
     A = np.ones(n_actions, dtype=float) * epsilon1 / n_actions
-    best_action = np.argmax([state for a in range(n_actions)])
+    best_action = np.argmax([q for a in range(n_actions)])
     A[best_action] += (1.0 - epsilon1)
-    sample = np.random.choice(n_actions, p=A)
-    return sample
+    action = np.random.choice(n_actions, p=A)
+    return action
 
 
 def linear_sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
@@ -26,12 +26,17 @@ def linear_sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
         done = False;
         while not done:
             action = epsilon_greedy(q, epsilon[i], env.n_actions)  # Get action for current state
-            state_, reward, done = env.step(action)  # Step into next environment
-            q_ = state_.dot(theta)  # Get Value for next state
+            features_, reward, done = env.step(action)  # Step into next environment
+
+            q_ = features_.dot(theta)  # Get Value for next state
             action_ = epsilon_greedy(q_, epsilon[i], env.n_actions)  # Find out next action from policy for state_
+
             target = reward + gamma * q_[action_]
             td_error = q - target
             dw = td_error.dot(features)
             theta -= eta[i] * dw
-
+            if done:
+                break
+                # update our state
+            features = features_
     return theta
